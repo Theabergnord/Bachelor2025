10c60f7dec192a34474468c715b6536d3b5c25f1
emilycostas <emilyenger@msn.com>
2025-03-26 14:54:31 +0100

    endret filnavn

--- DecisionTree/app/(tabs)/_layout.jsx
+++ DecisionTree/app/(tabs)/_layout.jsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="start"
+        name="index"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
